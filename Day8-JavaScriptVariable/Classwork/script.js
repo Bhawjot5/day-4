03401233e4bfc79859026f4cde8f0d62b4f27b5b
Deepak Atariya <deepakkumaratariya@gmail.com>
2020-09-23 07:33:33 +0530

    Day 8 completed

--- Day8-JavaScriptVariable/Classwork/script.js
+++ Day8-JavaScriptVariable/Classwork/script.js
@@ -10,3 +10,44 @@ function someFun() {
     heading.style.color = "red";
 
 }
+
+/**
+ * 
+ * Math elements:
+ * 1. Constants : anything that we cannot change; no updation allowed;
+ * 2. variables : anything that we can change according to our requirement; 
+ * A = 10 
+ * B = 5
+ * C = A + B 
+ * C = 15
+ * 
+ * A = 20
+ * C = A - B
+ * C = 15
+ * 
+ * B = 40
+ * 
+ * C = A * B
+ * C = 800
+ * 
+ * C = C * A
+ * C = 16000
+ * 
+ * What are the variables ?
+ * A, B and C are the variables, because they are getting changed after each operation/case.
+ * 
+ * Types of variables ?
+ * 1. Independent variable 
+ * 2. Dependent variable
+ * 
+ * A = Independent
+ * B = Independent 
+ * C = Dependent
+ * 
+ * 1. What is variable in JavaScript?
+ * Ans : variables in JavaScript are memory blocks. Wherein we put numbers, characters, etc.
+ * 
+ *  
+ *  
+ *
+ */
